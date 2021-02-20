bc8014e9bec61c9c179daa66c40f6aa0af82e790
WldrNerd <78234628+WldrNerd@users.noreply.github.com>
2021-02-20 17:20:17 -0300

    Adicionados arquivos via upload

---
+++ Teste GitHub/src/br/com/wldr/git/TesteGit.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package br.com.wldr.git;
+
+/**
+ * @author Wldr
+ * Exemplo de integração com o GitHub
+ */
+public class TesteGit {
+
+	public static void main(String[] args) {
+		System.out.println("Teste GitHub");
+
+	}
+
+}
